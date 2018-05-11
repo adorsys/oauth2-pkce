f5202d766d05c73c86093fbefa113efb8cb69d5e
Boris Skert <boris.skert@adorsys.de>
2018-05-11 07:59:42 +0200

    clean up code (Base64 url encoder)

--- oauth2-pkce/spring-oauth2-pkce/src/main/java/de/adorsys/oauth2/pkce/util/Base64Encoder.java
+++ oauth2-pkce/spring-oauth2-pkce/src/main/java/de/adorsys/oauth2/pkce/util/Base64Encoder.java
@@ -2,26 +2,20 @@ package de.adorsys.oauth2.pkce.util;
 
 import de.adorsys.oauth2.pkce.basetypes.ByteArray;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class Base64Encoder {
+
+    private static final Base64.Encoder BASE64_URL_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
     public String toBase64(ByteArray byteArray) {
         return toBase64(byteArray.getValue());
     }
 
     private String toBase64(byte[] bytes) {
-        byte[] encodedBase64InBytes = Base64.getEncoder().encode(bytes);
+        byte[] encodedBase64InBytes = BASE64_URL_ENCODER.encode(bytes);
 
-        String base64AsString = new String(encodedBase64InBytes);
-
-        base64AsString = base64AsString.split("=")[0];
-        base64AsString = base64AsString.replace('+', '-');
-        base64AsString = base64AsString.replace('/', '_');
-
-        return base64AsString;
+        return new String(encodedBase64InBytes);
     }
 
 }
