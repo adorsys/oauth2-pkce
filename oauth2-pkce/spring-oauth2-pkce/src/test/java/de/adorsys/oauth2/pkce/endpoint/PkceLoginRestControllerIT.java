d3c30abd85bfa95b6f8dafab6fccf98bb4e288ec
Boris Skert <boris.skert@adorsys.de>
2019-01-20 10:40:51 +0100

    extend integration-tests for login-endpoint with referer

--- oauth2-pkce/spring-oauth2-pkce/src/test/java/de/adorsys/oauth2/pkce/endpoint/PkceLoginRestControllerIT.java
+++ oauth2-pkce/spring-oauth2-pkce/src/test/java/de/adorsys/oauth2/pkce/endpoint/PkceLoginRestControllerIT.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.context.ConfigFileApplicationContextInitial
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -33,7 +34,10 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.text.IsEmptyString.isEmptyString;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
@@ -57,8 +61,14 @@ public class PkceLoginRestControllerIT {
     @Autowired
     UserAgentStateService userAgentStateService;
 
+    @Autowired
+    Environment environment;
+
     @MockBean
     Oauth2PkceFactory oauth2PkceFactory;
+
+    private String localServerAddress;
+    private String localServerTokenEndpointAddress;
 
     @Before
     public void setup() throws Exception {
@@ -68,6 +78,9 @@ public class PkceLoginRestControllerIT {
         CodeVerifier codeVerifier = new CodeVerifier(CODE_VERIFIER);
         given(oauth2PkceFactory.generateCodeVerifier()).willReturn(codeVerifier);
         given(oauth2PkceFactory.createCodeChallenge(codeVerifier)).willReturn(new CodeChallenge(CODE_CHALLENGE));
+
+        localServerAddress = "http://localhost:" + environment.getProperty("local.server.port");
+        localServerTokenEndpointAddress = localServerAddress + "/oauth2/token";
     }
 
     @Test
@@ -103,6 +116,64 @@ public class PkceLoginRestControllerIT {
         assertThat(queryParams.get("response_type"), is(equalTo("code")));
         assertThat(queryParams.get("response_mode"), is(equalTo("query")));
         assertThat(queryParams.get("redirect_uri"), is(equalTo("my_login_redirect_uri")));
+        assertThat(queryParams.get("code_challenge"), is(equalTo(CODE_CHALLENGE)));
+        assertThat(queryParams.get("nonce"), is(equalTo(NONCE)));
+        assertThat(queryParams.get("state"), is(equalTo(STATE)));
+    }
+
+    @Test
+    public void shouldRedirectToIdpLoginPageWithReferer() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Referer", "my_custom_referer");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+
+        ResponseEntity<String> response = restTemplate.exchange("/oauth2/login", HttpMethod.GET, new HttpEntity<>(body, headers), String.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
+        HttpHeaders responseHeaders = response.getHeaders();
+        URI redirectLocation = responseHeaders.getLocation();
+
+        assertThat(redirectLocation.getScheme(), is(equalTo("http")));
+        assertThat(redirectLocation.getHost(), is(equalTo("localhost")));
+        assertThat(redirectLocation.getPath(), is(equalTo("/auth/realms/moped/protocol/openid-connect/auth")));
+
+        MultiValueMap<String, HttpCookie> responseCookies = UriCookieUtils.parseCookiesAsMap(responseHeaders);
+        assertThat(responseCookies.size(), is(equalTo(2)));
+
+        List<HttpCookie> codeVerifierCookies = responseCookies.get(TokenConstants.CODE_VERIFIER_COOKIE_NAME);
+        assertThat(codeVerifierCookies, hasSize(1));
+
+        HttpCookie codeVerifierCookie = codeVerifierCookies.get(0);
+        assertThat(codeVerifierCookie.getName(), is(equalTo(TokenConstants.CODE_VERIFIER_COOKIE_NAME)));
+        assertThat(codeVerifierCookie.getPath(), is(equalTo("/oauth2/token")));
+        assertThat(codeVerifierCookie.getMaxAge(), is(equalTo(3600L)));
+        assertThat(codeVerifierCookie.getValue(), is(equalTo(CODE_VERIFIER)));
+
+        List<HttpCookie> userAgentStateCookies = responseCookies.get(TokenConstants.USER_AGENT_STATE_COOKIE_NAME);
+        assertThat(userAgentStateCookies, hasSize(1));
+
+        HttpCookie userAgentStateCookie = userAgentStateCookies.get(0);
+        assertThat(userAgentStateCookie.getName(), is(equalTo(TokenConstants.USER_AGENT_STATE_COOKIE_NAME)));
+        assertThat(userAgentStateCookie.getPath(), is(equalTo("/oauth2/token")));
+        assertThat(userAgentStateCookie.getMaxAge(), is(equalTo(3600L)));
+
+        String userAgentStateValue = userAgentStateCookie.getValue();
+        assertThat(userAgentStateValue, is(not(isEmptyString())));
+
+        UserAgentStateService.UserAgentState userAgentState = userAgentStateService.readUserAgentState(userAgentStateValue);
+        assertThat(userAgentState.getRedirectUri(), is(equalTo(localServerTokenEndpointAddress)));
+        assertThat(userAgentState.getUserAgentPage(), is(equalTo("my_custom_referer")));
+
+        Map<String, String> queryParams = UriQueryUtils.getQueryMap(redirectLocation.getQuery());
+        assertThat(queryParams.size(), is(equalTo(9)));
+        assertThat(queryParams.get("client_id"), is(equalTo("moped-client")));
+        assertThat(queryParams.get("scope"), is(equalTo("openid")));
+        assertThat(queryParams.get("code_challenge_method"), is(equalTo("S256")));
+        assertThat(queryParams.get("response_type"), is(equalTo("code")));
+        assertThat(queryParams.get("response_mode"), is(equalTo("query")));
+        assertThat(queryParams.get("redirect_uri"), is(equalTo(localServerTokenEndpointAddress)));
         assertThat(queryParams.get("code_challenge"), is(equalTo(CODE_CHALLENGE)));
         assertThat(queryParams.get("nonce"), is(equalTo(NONCE)));
         assertThat(queryParams.get("state"), is(equalTo(STATE)));
--- oauth2-pkce/spring-oauth2-pkce/src/test/java/de/adorsys/utils/UriCookieUtils.java
+++ oauth2-pkce/spring-oauth2-pkce/src/test/java/de/adorsys/utils/UriCookieUtils.java
@@ -1,12 +1,26 @@
 package de.adorsys.utils;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.net.HttpCookie;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UriCookieUtils {
+
+    public static MultiValueMap<String, HttpCookie> parseCookiesAsMap(HttpHeaders responseHeaders) {
+        MultiValueMap<String, HttpCookie> map = new LinkedMultiValueMap<>();
+
+        List<HttpCookie> httpCookies = parseCookiesToList(responseHeaders);
+
+        for(HttpCookie httpCookie : httpCookies) {
+            map.add(httpCookie.getName(), httpCookie);
+        }
+
+        return map;
+    }
 
     public static List<HttpCookie> parseCookiesToList(HttpHeaders responseHeaders) {
         List<String> cookieHeaderValues = responseHeaders.get("Set-Cookie");
