68bd4275f27d760a9c772f4a3983ad95ffaa1647
Boris Skert <boris.skert@adorsys.de>
2019-01-21 23:24:39 +0100

    run integration-tests on CI pipeline

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --settings .travis/settings.xml --file ./oauth2-pkce/pom.xml clean verify -DskipTests -B -V
+mvn --settings .travis/settings.xml --file ./oauth2-pkce/pom.xml clean verify -B -V
 
 .travis/build_client.sh
